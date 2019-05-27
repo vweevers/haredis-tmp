9fe4fdf299200d0f2fe28668767a4d72fc7c75e1
Vincent Weevers <mail@vincentweevers.nl>
2019-05-27 09:46:39 +0200

    Get rid of globals in tests

--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,11 @@
-/* global it describe assert assertPorts assertNoPorts haredis tmp existsSync */
+/* global it describe */
+
+const tmp = require('..')
+const haredis = require('haredis')
+const Redis = require('ioredis')
+const assert = require('assert')
+const existsSync = require('fs').existsSync
+const net = require('net')
 
 describe('basic test', function () {
   let ports = [6380, 6381, 6382]
@@ -13,11 +20,11 @@ describe('basic test', function () {
       p = arg1
       shutdown = arg2
       servers = arg3
-      assert.equal(typeof shutdown, 'function')
-      assert.equal(typeof p, 'string')
+      assert.strictEqual(typeof shutdown, 'function')
+      assert.strictEqual(typeof p, 'string')
       assert(~p.indexOf('haredis-tmp'))
       assert(existsSync(p))
-      assert.equal(Object.keys(servers).length, 3)
+      assert.strictEqual(Object.keys(servers).length, 3)
       done()
     })
   })
@@ -29,7 +36,7 @@ describe('basic test', function () {
   it('connect to cluster', function (done) {
     client = haredis.createClient(ports, { log_level: 0 })
     client.once('connect', function () {
-      assert.equal(client.up.length, 3)
+      assert.strictEqual(client.up.length, 3)
       done()
     })
   })
@@ -50,3 +57,92 @@ describe('basic test', function () {
     assert(!existsSync(p))
   })
 })
+
+describe('password test with ioredis', function () {
+  let port = 6380
+  let shutdown
+  let client
+  let p
+  let servers
+
+  it('create an instance', function (done) {
+    tmp([port], { password: 'qwerty' }, function (err, arg1, arg2, arg3) {
+      assert.ifError(err)
+      p = arg1
+      shutdown = arg2
+      servers = arg3
+      assert.strictEqual(typeof shutdown, 'function')
+      assert.strictEqual(typeof p, 'string')
+      assert(~p.indexOf('haredis-tmp'))
+      assert(existsSync(p))
+      assert.strictEqual(Object.keys(servers).length, 1)
+      done()
+    })
+  })
+
+  it('ports accessible', function (done) {
+    assertPorts([port], done)
+  })
+
+  it('connect to instance', function (done) {
+    client = new Redis({
+      port: port,
+      host: '127.0.0.1',
+      family: 4,
+      password: 'qwerty'
+    })
+
+    client.once('connect', function () {
+      done()
+    })
+  })
+
+  it('quit client', function (done) {
+    client.quit(done)
+  })
+
+  it('shutdown cluster', function (done) {
+    shutdown(done)
+  })
+
+  it('ports not accessible', function (done) {
+    assertNoPorts([port], done)
+  })
+
+  it('no files left over', function () {
+    assert(!existsSync(p))
+  })
+})
+
+function assertPorts (ports, done) {
+  let latch = ports.length
+
+  ports.forEach(function (port) {
+    const socket = net.createConnection(port)
+
+    socket.once('error', done)
+    socket.once('connect', function () {
+      socket.once('close', function () {
+        if (!--latch) done()
+      })
+
+      socket.end()
+    })
+  })
+}
+
+function assertNoPorts (ports, done) {
+  let latch = ports.length
+
+  ports.forEach(function (port) {
+    const socket = net.createConnection(port)
+
+    socket.once('error', function () {
+      if (!--latch) done()
+    })
+
+    socket.once('connect', function () {
+      assert.fail('connected', 'not connected')
+    })
+  })
+}
